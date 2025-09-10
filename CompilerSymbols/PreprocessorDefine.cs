6f968eebde4ed914f50b62fe118f63ffde7220ed
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2025-09-10 16:50:59 +0200

    Update PreprocessorDefine.cs

--- CompilerSymbols/PreprocessorDefine.cs
+++ CompilerSymbols/PreprocessorDefine.cs
@@ -8,6 +8,7 @@
 
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.Build;
 
 namespace Quirks.Audio
 {
@@ -19,7 +20,12 @@ namespace Quirks.Audio
         [InitializeOnLoadMethod]
         public static void AddDefineSymbols()
         {
+#if UNITY_2021_2_OR_NEWER
+            string currentDefines = PlayerSettings.GetScriptingDefineSymbols(NamedBuildTarget.FromBuildTargetGroup(EditorUserBuildSettings.selectedBuildTargetGroup));
+#else
             string currentDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+#endif
+
             HashSet<string> defines = new HashSet<string>(currentDefines.Split(';'))
             {
                 "CLOCKWORK_AUDIO"
@@ -30,7 +36,12 @@ namespace Quirks.Audio
             string newDefines = string.Join(";", defines);
             if (newDefines != currentDefines)
             {
+#if UNITY_2021_2_OR_NEWER
+                PlayerSettings.SetScriptingDefineSymbols(UnityEditor.Build.NamedBuildTarget.FromBuildTargetGroup(EditorUserBuildSettings.selectedBuildTargetGroup), newDefines);
+#else
+                // Deprecated in Unity 2023.1
                 PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, newDefines);
+#endif
             }
         }
     }
